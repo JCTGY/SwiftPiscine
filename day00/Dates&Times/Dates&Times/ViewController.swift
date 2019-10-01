e09316ddb01583d97c038f332c7eb830b6e79107
JCTGY <fan3510@gmail.com>
2019-10-01 08:56:57 -0700

    fix landscape

---
+++ day00/Dates&Times/Dates&Times/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dates&Times
+//
+//  Created by jeffrey chiang on 10/1/19.
+//  Copyright © 2019 jeffrey chiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
